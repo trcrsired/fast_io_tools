37a83558f7d470dffbad36390a7912fced8e20f6
expnkx <unlvsur@live.com>
2021-03-25 15:35:51 -0400

    res

--- afpf/socket_domain.cc
+++ afpf/socket_domain.cc
@@ -1,30 +1,8 @@
 #include"../../fast_io/include/fast_io.h"
 #include"../../fast_io/include/fast_io_device.h"
+#include"token.h"
 #include<map>
 
-struct parse_line_result
-{
-std::size_t a=-1;
-std::size_t b=-1;
-std::size_t c=-1;
-std::size_t d=-1;
-};
-
-inline constexpr std::size_t skip_tab(std::u8string_view token) noexcept
-{
-	std::size_t i{};
-	for(;i!=token.size()&&token[i]==u8'\t';++i);
-	return i;
-}
-
-inline constexpr parse_line_result parse(std::u8string_view view) noexcept
-{
-	auto atoken{find_next_token(view)};
-	std::u8string_view atoken_vw(view.substr(atoken));
-	if(atoken)[[unlikely]]
-		fast_io::fast_terminate();
-	
-}
 
 int main(int argc,char** argv) noexcept
 {
@@ -36,30 +14,43 @@ int main(int argc,char** argv) noexcept
 	}
 	fast_io::u8ibuf_file ibf(argv[1]);
 	using namespace std::string_view_literals;
-	std::map<std::u8string,std::u8string> set;
+	std::map<std::u8string,std::u8string> map;
 	for(auto line:line_generator(ibf))
 	{
 		if(line.empty())
 			continue;
-		if(a!=u8"#define"sv||b.size()<4)
+		auto tok{parse_token(line)};
+		std::u8string_view fist_token(line.cbegin()+tok.first.start,line.cbegin()+tok.first.last);
+		if(fist_token!=u8"#define"sv||(tok.second.last-tok.second.start<static_cast<std::size_t>(4)))
 		{
-			perrln(fast_io::u8err(),u8"unknown:",a,u8" ",b,u8" ",c);
+			perrln(fast_io::u8err(),u8"unknown:",line);
 			continue;
 		}
-		std::u8string sstr(b.substr(3));
-		for(auto & e : sstr)
+		std::u8string second(line.data()+tok.second.start+3,line.data()+tok.second.last);
+		for(auto & e : second)
 			e=fast_io::char_category::to_c_lower(e);
-		if(set.contains(sstr)||set.contains(sstr))
+		if(static_cast<std::size_t>(3)<tok.third.last-tok.third.start)
 		{
-			continue;
+			std::u8string third(line.cbegin()+tok.third.start+3,line.cbegin()+tok.third.last);
+			for(auto & e : third)
+				e=fast_io::char_category::to_c_lower(e);
+			if(map.contains(third))
+				continue;
 		}
-		set.emplace(std::move(sstr));
+		map.try_emplace(std::move(second),line.cbegin()+tok.fourth.start,line.cbegin()+tok.fourth.last);
 	}
-	fast_io::u8obuf_file obf(argv[2]);
-	std::size_t i{};
-	for(auto const& e : set)
+	std::u8string buffer;
+	fast_io::u8ostring_ref ref{&buffer};
+	
+	for(std::size_t i{};auto const& e : map)
 	{
-		println(obf,e,u8"=",i);
+		print(ref,e.first,u8"\t=\t",i);
+		if(!e.second.empty())
+			print(ref,u8",\t",e.second);
+		println(ref);
 		++i;
 	}
+
+	fast_io::u8obuf_file obf(argv[2]);
+	print(obf,auto_indent(buffer));
 }
