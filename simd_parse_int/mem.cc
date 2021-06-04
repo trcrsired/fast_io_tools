05a79fe7a5fc33a78ce3932912c8d4d5ce80deb6
expnkx <unlvsur@live.com>
2021-06-04 00:40:08 -0400

    backup

--- simd_parse_int/mem.cc
+++ simd_parse_int/mem.cc
@@ -76,16 +76,23 @@ inline char8_t detect_length(char8_t const* buffer) noexcept
 	return std::countr_one(v);
 }
 
-inline ::fast_io::parse_result<char8_t const*> sse_parse(char8_t const* buffer,char8_t const* buffer_end,std::uint64_t &res) noexcept
+struct simd_parse_result
+{
+	std::size_t digits;
+	fast_io::parse_code code;
+};
+
+inline simd_parse_result sse_parse(char8_t const* buffer,char8_t const* buffer_end,std::uint64_t &res) noexcept
 {
 	__m128i chunk = _mm_loadu_si128(reinterpret_cast<__m128i const*>(buffer));
 	__m128i const t0 = _mm_sub_epi8(chunk, _mm_set1_epi8(176));
 	__m128i const mask = _mm_cmplt_epi8(t0, _mm_set1_epi8(-118));
 	std::uint16_t v{static_cast<std::uint16_t>(_mm_movemask_epi8(mask))};
 	std::uint32_t digits{static_cast<std::uint32_t>(std::countr_one(v))};
+	using fast_io::parse_code;
 	if(digits==0)
 	{
-		return {buffer,parse_code::invalid};
+		return {0,parse_code::invalid};
 	}
 	std::uint32_t rshift_digits{16-digits};
 	constexpr char8_t zero_constant{u8'0'};
@@ -95,81 +102,96 @@ inline ::fast_io::parse_result<char8_t const*> sse_parse(char8_t const* buffer,c
 	chunk = _mm_madd_epi16(chunk, _mm_set_epi16(1,100,1,100,1,100,1,100));
 	chunk = _mm_packus_epi32(chunk, chunk);
 	chunk = _mm_madd_epi16(chunk,_mm_set_epi16(0,0,0,0,1,10000,1,10000));
-	std::uint64_t result{((chunk[0] & 0xffffffff) * 100000000) + (chunk[0] >> 32)};
+	std::uint64_t result{static_cast<std::uint64_t>(((chunk[0] & 0xffffffff) * 100000000) + (chunk[0] >> 32))};
 
 	if(digits==16)[[unlikely]]
 	{
 		__m128i chunk1 = _mm_loadu_si128(reinterpret_cast<__m128i const*>(buffer+16));
 		__m128i const t0_1 = _mm_sub_epi8(chunk1, _mm_set1_epi8(176));
 		__m128i const mask_1 = _mm_cmplt_epi8(t0_1, _mm_set1_epi8(-118));
-		std::uint16_t v1{static_cast<std::uint16_t>(_mm_movemask_epi8(mask1))};
+		std::uint16_t v1{static_cast<std::uint16_t>(_mm_movemask_epi8(mask_1))};
 		std::uint32_t digits1{static_cast<std::uint32_t>(std::countr_one(v1))};
 //18446744073709551615 20 digits
 		switch(digits1)
 		{
 		case 3:
 		{
-			res=result*1000ULL+static_cast<std::uint64_t>(buffer[16]-zero)*100ULL+static_cast<std::uint64_t>(buffer[17]-zero)*10ULL
-			+static_cast<std::uint64_t>(buffer[18]-zero);
-			return {buffer+19,parse_code::ok};
+			res=result*1000ULL+static_cast<std::uint64_t>(buffer[16]-zero_constant)*100ULL+static_cast<std::uint64_t>(buffer[17]-zero_constant)*10ULL
+			+static_cast<std::uint64_t>(buffer[18]-zero_constant);
+			return {19,parse_code::ok};
 		}
 		case 2:
 		{
-			res=result*100+static_cast<std::uint64_t>(buffer[16]-zero)*10ULL+static_cast<std::uint64_t>(buffer[17]-zero);
-			return {buffer+18,parse_code::ok};
+			res=result*100+static_cast<std::uint64_t>(buffer[16]-zero_constant)*10ULL+static_cast<std::uint64_t>(buffer[17]-zero_constant);
+			return {18,parse_code::ok};
 		}
 		case 1:
 		{
-			res=result*10+static_cast<std::uint64_t>(buffer[16]-zero);
-			return {buffer+17,parse_code::ok};
+			res=result*10+static_cast<std::uint64_t>(buffer[16]-zero_constant);
+			return {17,parse_code::ok};
 		}
 		case 0:
 		{
 			res=result;
-			return {buffer+16,parse_code::ok};
+			return {16,parse_code::ok};
 		}
 		case 4:
 		{
 			constexpr std::uint64_t risky_value{UINT64_MAX/10000ULL};
 			constexpr std::uint64_t risky_mod{UINT64_MAX%10000ULL};
 			if(result>risky_value)
-				return {buffer+20,parse_code::overflow};
-			std::uint64_t partial{static_cast<std::uint64_t>(buffer[16]-zero)*1000ULL+static_cast<std::uint64_t>(buffer[17]-zero)*100ULL
-			+static_cast<std::uint64_t>(buffer[18]-zero)*10ULL+static_cast<std::uint64_t>(buffer[19]-zero)};
+				return {20,parse_code::overflow};
+			std::uint64_t partial{static_cast<std::uint64_t>(buffer[16]-zero_constant)*1000ULL+static_cast<std::uint64_t>(buffer[17]-zero_constant)*100ULL
+			+static_cast<std::uint64_t>(buffer[18]-zero_constant)*10ULL+static_cast<std::uint64_t>(buffer[19]-zero_constant)};
 			
 			if(result==risky_value&&risky_mod<partial)
-				return {buffer+20,parse_code::overflow};
+				return {20,parse_code::overflow};
 			res=result*10000ULL+partial;
-			return {buffer+20,parse_code::okay};
+			return {20,parse_code::ok};
 		}
 		case 16:
 		{
 			auto it{buffer+32};
-			for(it;16<=buffer_end-it;)
+			for(;16<=buffer_end-it;it+=16)
 			{
 				__m128i chunk2 = _mm_loadu_si128(reinterpret_cast<__m128i const*>(buffer+16));
 				__m128i const t0_2 = _mm_sub_epi8(chunk1, _mm_set1_epi8(176));
 				__m128i const mask_2 = _mm_cmplt_epi8(t0_1, _mm_set1_epi8(-118));
-				std::uint16_t v2{static_cast<std::uint16_t>(_mm_movemask_epi8(mask2))};
+				std::uint16_t v2{static_cast<std::uint16_t>(_mm_movemask_epi8(mask_2))};
 				std::uint32_t digits2{static_cast<std::uint32_t>(std::countr_one(v2))};
+				if(digits2!=16)
+					return {static_cast<std::size_t>(it-buffer),parse_code::overflow};
 			};
+			if(buffer_end==it)
+				return {static_cast<std::size_t>(it-buffer),parse_code::overflow};
+			auto it2{buffer_end-16};
+			__m128i chunk2 = _mm_loadu_si128(reinterpret_cast<__m128i const*>(it2));
+			__m128i const t0_2 = _mm_sub_epi8(chunk1, _mm_set1_epi8(176));
+			__m128i const mask_2 = _mm_cmplt_epi8(t0_1, _mm_set1_epi8(-118));
+			std::uint16_t v2{static_cast<std::uint16_t>(_mm_movemask_epi8(mask_2))};
+			std::uint32_t digits2{static_cast<std::uint32_t>(std::countr_one(v2))};
+			return {static_cast<std::size_t>(buffer_end-buffer+digits2-16),parse_code::overflow};
 		}
 		default:
 		{
-			return {buffer+digits+16,parse_code::overflow};
+			return {digits+16,parse_code::overflow};
 		}
 		}
 	}
 	res=result;
-	return {buffer+digits,parse_code::ok};
+	return {digits,parse_code::ok};
 }
 
 #endif
 
 int main()
 {
-	char8_t buffer[]=u8"99999999999 1                                                                                          ";
+	char8_t buffer[]=u8"18446744073709551615 1                                                                                          ";
 //	std::uint64_t digit(parse_8_chars(buffer));
-	println(sse_parse(buffer));
-	return 0;
+	std::uint64_t digit{};
+	auto [digits,ec]=sse_parse(buffer,buffer+sizeof(buffer),digit);
+	if(ec!=fast_io::parse_code::ok)
+		println("failed:",ec," digits:",digits);
+	else
+		println(digit);
 }
