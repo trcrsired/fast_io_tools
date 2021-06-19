04d7911c9bb37a6dd1ccec756f66f3e065d5ddba
expnkx <unlvsur@live.com>
2021-06-18 23:44:03 -0400

    pe

--- parser/parse.cc
+++ parser/parse.cc
@@ -10,5 +10,13 @@ int main(int argc,char** argv)
 		return 1;
 	}
 	fast_io::native_file_loader loader(argv[1]);
-	pe_file_format::pe_metadata meta{pe_file_format::deserialize(loader.begin(),loader.end())};
+	pe_file_format::pe_file pe{pe_file_format::deserialize(loader.begin(),loader.end())};
+	println("PE header magic:",pe.peheader.mMagic,
+		"\nmachine type:",fast_io::mnp::hex(pe.peheader.mMachine),
+		"\noptionalheader magic:",pe.peoptionalheader.mMagic,
+		"\nmajor operating system version:",pe.peoptionalheader.mMajorOperatingSystemVersion,
+		"\nminor operating system version:",pe.peoptionalheader.mMinorOperatingSystemVersion,
+		"\nmajor subsystem version:",pe.peoptionalheader.mMajorSubsystemVersion,
+		"\nminor subsystem version:",pe.peoptionalheader.mMinorSubsystemVersion,
+		"\ncheck sum:",pe.peoptionalheader.mCheckSum);
 }
--- parser/pe_file.h
+++ parser/pe_file.h
@@ -5,27 +5,28 @@ namespace pe_file_format
 {
 
 template<typename T>
-inline T const& dereference(char const* ptr,char const* last)
+inline constexpr T const& dereference(char const* ptr,char const* last) noexcept
 {
 	if(static_cast<std::size_t>(last-ptr)<sizeof(T))
 		panic();
 	return *reinterpret_cast<T const*>(ptr);
 }
 
-inline char const* check_ptr_advance(char const* ptr,char const* last,std::size_t pos)
+inline  constexpr char const* check_ptr_advance(char const* ptr,char const* last,std::size_t pos) noexcept
 {
 	if(ptr+pos>last)
 		panic();
-	return ptr;
+	return ptr+pos;
 }
 
-struct pe_metadata
+struct pe_file
 {
-	image_dos_header const& dosheader;
-	pe_header const& peheader;
+	image_dos_header const& dosheader{};
+	pe_header const& peheader{};
+	pe32_optional_header const& peoptionalheader{};
 };
 
-inline pe_metadata deserialize(char const* first,char const* last)
+inline constexpr pe_file deserialize(char const* first,char const* last) noexcept
 {
 	auto& dosheader{dereference<image_dos_header>(first,last)};
 	if(dosheader.e_magic!=0x5A4D)
@@ -33,7 +34,10 @@ inline pe_metadata deserialize(char const* first,char const* last)
 	auto& peheader{dereference<pe_header>(check_ptr_advance(first,last,dosheader.e_lfanew),last)};
 	if(peheader.mMagic!=0x4550)
 		panicln("pe header magic is not 0x00004550 (PE00) it is 0x",fast_io::mnp::hex(peheader.mMagic));
-	return {dosheader,peheader};
+
+	auto& peoptionalheader{dereference<pe32_optional_header>(check_ptr_advance(first,last,dosheader.e_lfanew+sizeof(peheader)),last)};
+
+	return {dosheader,peheader,peoptionalheader};
 }
 
 }
