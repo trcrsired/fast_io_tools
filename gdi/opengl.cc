a1e69b77905f735a8d154e41835da79485ee05a6
expnkx <unlvsur@live.com>
2021-10-08 06:17:19 -0400

    asfasfasf

---
+++ gdi/opengl.cc
@@ -0,0 +1,63 @@
+#include<fast_io.h>
+#include<fast_io_device.h>
+#include"win32gdi.h"
+
+/*
+https://docs.microsoft.com/en-us/windows/win32/learnwin32/your-first-windows-program
+*/
+
+int main()
+#ifdef __cpp_exceptions
+try
+#endif
+{
+	using namespace fast_io::win32;
+	fast_io::win32::wndclass wc{
+	.lpfnWndProc=[](void* hwnd,std::uint32_t msg,std::uintptr_t wparam,std::intptr_t lparam) noexcept -> std::intptr_t
+	{
+		switch(msg)
+		{
+		case 2://WM_DESTROY
+			PostQuitMessage(0);
+			return 0;
+		default:
+		return fast_io::win32::DefWindowProcW(hwnd,msg,wparam,lparam);
+		}
+	},
+	.hInstance=fast_io::win32::GetProcessInstanceHandle(),
+	.lpszClassName=u"OpenGL"
+	};
+	if(!fast_io::win32::RegisterClassW(__builtin_addressof(wc)))
+		fast_io::throw_win32_error();
+	fast_io::win32::win32_window_file window(0,u"OpenGL",u"OpenGL",
+		static_cast<std::uint32_t>(fast_io::win32::window_styles::overlappedwindow),
+		fast_io::win32::cw_usedefault,
+		fast_io::win32::cw_usedefault,
+		fast_io::win32::cw_usedefault,
+		fast_io::win32::cw_usedefault,
+		nullptr,nullptr,fast_io::win32::GetProcessInstanceHandle(),nullptr);
+	
+	auto window_dc{get_dc(window)};
+	choose_opengl_pixel_format(window_dc);
+	fast_io::win32::win32_glrc gl{window_dc};
+	wgl_make_current(window_dc,gl);
+	
+	fast_io::win32::ShowWindow(window.hwnd,10);
+	for(fast_io::win32::msg msg{};;)
+	{
+		int val{fast_io::win32::GetMessageW(__builtin_addressof(msg),nullptr,0,0)};
+		if(val==-1)
+			fast_io::throw_win32_error();
+		else if(val==0)
+			break;
+		fast_io::win32::TranslateMessage(__builtin_addressof(msg));
+		fast_io::win32::DispatchMessageW(__builtin_addressof(msg));
+	}
+}
+#ifdef __cpp_exceptions
+catch(fast_io::win32_error ec)
+{
+	perrln(ec);
+	return 1;
+}
+#endif
--- gdi/win32gdi.h
+++ gdi/win32gdi.h
@@ -32,6 +32,8 @@ extern void* CreateWindowExW(std::uint32_t,char16_t const*,char16_t const*,std::
 [[gnu::dllimport,gnu::stdcall]]
 extern std::intptr_t DefWindowProcW(void*,std::uint32_t,std::uintptr_t,std::intptr_t) noexcept __asm__("DefWindowProcW");
 
+[[gnu::dllimport,gnu::stdcall]]
+extern void* GetDC(void*) noexcept __asm__("GetDC");
 /*
 Referenced from https://docs.microsoft.com/en-us/windows/win32/winmsg/window-styles
 */
@@ -62,6 +64,37 @@ visible=0x10000000L,
 vscroll=0x00200000L,
 overlappedwindow=overlapped|caption|sysmenu|thickframe|minimizebox|maximizebox
 };
+
+struct pixel_format_descriptor
+{
+std::uint16_t  nSize;
+std::uint16_t  nVersion;
+std::uint32_t dwFlags;
+std::uint8_t  iPixelType;
+std::uint8_t  cColorBits;
+std::uint8_t  cRedBits;
+std::uint8_t  cRedShift;
+std::uint8_t  cGreenBits;
+std::uint8_t  cGreenShift;
+std::uint8_t  cBlueBits;
+std::uint8_t  cBlueShift;
+std::uint8_t  cAlphaBits;
+std::uint8_t  cAlphaShift;
+std::uint8_t  cAccumBits;
+std::uint8_t  cAccumRedBits;
+std::uint8_t  cAccumGreenBits;
+std::uint8_t  cAccumBlueBits;
+std::uint8_t  cAccumAlphaBits;
+std::uint8_t  cDepthBits;
+std::uint8_t  cStencilBits;
+std::uint8_t  cAuxBuffers;
+std::uint8_t  iLayerType;
+std::uint8_t  bReserved;
+std::uint16_t dwLayerMask;
+std::uint16_t dwVisibleMask;
+std::uint16_t dwDamageMask;
+};
+
 
 inline constexpr int cw_usedefault(0x80000000);
 
@@ -99,6 +132,167 @@ public:
 			DestroyWindow(this->hwnd);
 	}
 };
+
+struct win32_dc_observer
+{
+	void* hdc{};
+};
+
+namespace details
+{
+inline win32_dc_observer get_dc_throw(void* hwnd)
+{
+	if(hwnd==nullptr)
+		throw_win32_error();
+	return {GetDC(hwnd)};
+}
+}
+
+inline win32_dc_observer get_dc(win32_window_file const& h)
+{
+	return details::get_dc_throw(h.hwnd);
+}
+
+[[gnu::dllimport,gnu::stdcall]]
+extern void* wglCreateContext(void*) noexcept __asm__("wglCreateContext");
+
+[[gnu::dllimport,gnu::stdcall]]
+extern int wglDeleteContext(void*) noexcept __asm__("wglDeleteContext");
+
+[[gnu::dllimport,gnu::stdcall]]
+extern int wglCopyContext(void*,void*,std::uint32_t) noexcept __asm__("wglCopyContext");
+
+[[gnu::dllimport,gnu::stdcall]]
+extern int ChoosePixelFormat(void*,pixel_format_descriptor const*) noexcept __asm__("ChoosePixelFormat");
+
+
+[[gnu::dllimport,gnu::stdcall]]
+extern int SetPixelFormat(void*,int,pixel_format_descriptor const*) noexcept __asm__("SetPixelFormat");
+
+[[gnu::dllimport,gnu::stdcall]]
+extern int wglMakeCurrent(void*,void*) noexcept __asm__("wglMakeCurrent");
+
+namespace details
+{
+
+inline void* dup_glrc_context(void* glrc)
+{
+	void* new_glrc{wglCreateContext(glrc)};
+	if(new_glrc==nullptr)
+		throw_win32_error();
+	int ret{wglCopyContext(glrc,new_glrc,0x000FFFFF)};
+	if(!ret)
+	{
+		wglDeleteContext(new_glrc);
+		throw_win32_error();
+	}
+	return new_glrc;
+}
+
+}
+
+struct win32_glrc
+{
+	void* glrc{};
+	constexpr win32_glrc() noexcept = default;
+	win32_glrc(win32_dc_observer parameter):glrc(wglCreateContext(parameter.hdc))
+	{
+		if(glrc==nullptr)[[unlikely]]
+			throw_win32_error();
+	}
+	win32_glrc(::std::nullptr_t)=delete;
+	win32_glrc(win32_glrc const& other):glrc{::fast_io::win32::details::dup_glrc_context(other.glrc)}{}
+	win32_glrc& operator=(win32_glrc const& other)
+	{
+		auto temp{::fast_io::win32::details::dup_glrc_context(other.glrc)};
+		if(glrc)
+			wglDeleteContext(glrc);
+		glrc=temp;
+		return *this;
+	}
+	constexpr win32_glrc(win32_glrc&& other) noexcept:glrc{other.glrc}
+	{
+		other.glrc=nullptr;
+	}
+	win32_glrc& operator=(win32_glrc&& other) noexcept
+	{
+		if(__builtin_addressof(other)==this)
+			return *this;
+		if(glrc)
+			wglDeleteContext(glrc);
+		glrc=other.glrc;
+		other.glrc=nullptr;
+	}
+	void close()
+	{
+		if(glrc)
+		{
+			int ret{wglDeleteContext(glrc)};
+			glrc=nullptr;
+			if(ret)
+				throw_win32_error();
+		}
+	}
+	~win32_glrc()
+	{
+		if(glrc)
+			wglDeleteContext(glrc);
+	}
+};
+
+namespace details
+{
+
+inline void wgl_make_current_impl(void* hdc,void* hglrc)
+{
+	if(!wglMakeCurrent(hdc,hglrc))
+		throw_win32_error();
+}
+
+}
+
+inline constexpr pixel_format_descriptor opengl_pixel_descriptor
+{
+sizeof(pixel_format_descriptor),  //  size of this pfd  
+1,                     // version number  
+4 |   // support window  
+32 |   // support OpenGL  
+1,      // double buffered  
+0,         // RGBA type  
+24,                    // 24-bit color depth  
+0, 0, 0, 0, 0, 0,      // color bits ignored  
+0,                     // no alpha buffer  
+0,                     // shift bit ignored  
+0,                     // no accumulation buffer  
+0, 0, 0, 0,            // accum bits ignored  
+32,                    // 32-bit z-buffer      
+0,                     // no stencil buffer  
+0,                     // no auxiliary buffer  
+0,        // main layer  
+0,                     // reserved  
+0, 0, 0                // layer masks ignored  
+};
+
+
+inline void wgl_make_current(win32_dc_observer dc,win32_glrc const& w)
+{
+	::fast_io::win32::details::wgl_make_current_impl(dc.hdc,w.glrc);
+}
+
+inline auto choose_pixel_format(win32_dc_observer dc,pixel_format_descriptor const& ppfd)
+{
+	auto ret{ChoosePixelFormat(dc.hdc,__builtin_addressof(ppfd))};
+	if(!ret)
+		throw_win32_error();
+	return ret;
+}
+
+inline void choose_opengl_pixel_format(win32_dc_observer dc)
+{
+	auto format{choose_pixel_format(dc,opengl_pixel_descriptor)};
+	if(!SetPixelFormat(dc.hdc,format,__builtin_addressof(opengl_pixel_descriptor)))
+		throw_win32_error();
+}
 
 struct point
 {
@@ -152,7 +346,6 @@ extern int AppendMenuW(void*,std::uint32_t,std::uintptr_t,wchar_t const*) noexce
 
 [[gnu::dllimport,gnu::stdcall]]
 extern int AppendMenuA(void*,std::uint32_t,std::uintptr_t,char const*) noexcept __asm__("AppendMenuA");
-
 
 struct menu
 {
