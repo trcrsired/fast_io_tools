2286423c007e37cbadad57eed2ae8dbb37717798
expnkx <unlvsur@live.com>
2021-11-13 16:05:47 -0500

    codegen

--- sha512simd/simd_codegen.cc
+++ sha512simd/simd_codegen.cc
@@ -1,13 +1,12 @@
 #include<fast_io.h>
 #include<fast_io_device.h>
 
-int main()
+inline void code_gen(fast_io::u8obuf_file& obf,std::size_t magic)
 {
-	fast_io::u8obuf_file obf(u8"simd16_res.txt");
 	constexpr char8_t perm[]{u8'a',u8'b',u8'c',u8'd',u8'e',u8'f',u8'h',u8'g'};
 	using namespace fast_io::mnp;
 
-	for(std::size_t i{};i!=82;i+=2)
+	for(std::size_t i{};i<=80;i+=magic)
 	{
 		if(i==0)
 		{
@@ -40,8 +39,8 @@ u8R"abc(		simd.load(blocks_start+)abc",i*8u,u8R"abc();
 				print(obf,
 u8R"abc(		simd_temp.load(w+)abc",i-15,u8R"abc();
 		simd.load(w+)abc",i-2,u8R"abc();
-		simd=(((((simd>>42)^simd)>>13)^simd)>>6)^(((simd<<42)^simd)<<3);
-		simd_temp=(((((simd_temp>>1)^simd_temp)>>6)^simd_temp)>>1)^(((simd_temp<<7)^simd_temp)<<56);
+		simd16=(simd16>>19)^(simd16<<45)^(simd16>>61)^(simd16<<3)^(simd16>>6);
+		simd16_temp=(simd16_temp>>1)^(simd16_temp<<63)^(simd16_temp>>8)^(simd16_temp<<56)^(simd16_temp>>7);
 		simd.wrap_add_assign(simd_temp);
 		simd_temp.load(w)abc");
 		if(i!=16)
@@ -50,21 +49,35 @@ u8R"abc(		simd_temp.load(w+)abc",i-15,u8R"abc();
 		simd.wrap_add_assign(simd_temp);
 		simd_temp.load(w+)abc",i-7,u8R"abc();
 		simd.wrap_add_assign(simd_temp);
-		simd.store(w+)abc",i,u8R"abc();
-		simd_constants_load.load(K512+)abc",i,u8R"abc();
+)abc");
+		if(i+magic!=80)
+			print(obf,u8R"abc(		simd.store(w+)abc",i,u8");\n");
+		print(obf,u8R"abc(		simd_constants_load.load(K512+)abc",i,u8R"abc();
 		simd.wrap_add_assign(simd_constants_load);
 		simd.store(wt+)abc",i,u8");\n");
 			}
-			for(std::size_t j{};j!=2;++j)
+			for(std::size_t j{};j!=magic;++j)
 			{
-				print(obf,u8"\t\tsha512_scalar_round(wt[",i-2+j,u8"]");
+				std::size_t pos{i-magic+j};
+				print(obf,u8"\t\tsha512_scalar_round(wt[",i-magic+j,u8"]");
 				for(std::size_t k{};k!=8u;++k)
 				{
-					print(obf,u8",",chvw(u8'a'+static_cast<char8_t>(((k+8u-(i-2u+j))%8u)%8u)));
+					print(obf,u8",",chvw(u8'a'+static_cast<char8_t>(((k+8u-pos)%8u)%8u)));
 				}
 				print(obf,u8");\n");
 			}
-			println(obf);
+			if(i!=80)
+				println(obf);
 		}
 	}
+}
+
+int main()
+{
+	fast_io::u8obuf_file obf(u8"simd16_res.txt");
+	code_gen(obf,2);
+	obf.reopen(u8"simd32_res.txt");
+	code_gen(obf,4);
+	obf.reopen(u8"simd64_res.txt");
+	code_gen(obf,8);
 }
