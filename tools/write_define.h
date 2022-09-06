879fcc4dbc005cbe3e2fd3617dcccb3f041d4c01
trcrsired <uwgghhbcad@gmail.com>
2022-09-05 21:38:30 -0400

    def

--- tools/write_define.h
+++ tools/write_define.h
@@ -1,7 +1,10 @@
-inline constexpr std::byte* write_some_bytes_define(basic_posix_io_observer<char> ent,std::byte* first,std::byte* last)
+inline constexpr std::byte* write_some_bytes_define(posix_io_observer ent,std::byte* first,std::byte* last)
 {
-	return _write(end,first,last);
+	return write(ent.fd,first,static_cast<::std::size_t>(last-first))+first;
 }
+
+namespace details
+{
 
 template<typename Func>
 inline constexpr void write_all_common_impl(Func fnc,std::byte* first,std::byte* last) noexcept(noexcept(fnc(first,last)))
@@ -9,12 +12,68 @@ inline constexpr void write_all_common_impl(Func fnc,std::byte* first,std::byte*
 	for(;first!=last;first=fnc(first,last));
 }
 
+template<typename T>
+inline constexpr void decay_write_all_impl(T t,std::byte const* first,std::byte const* last)
+{
+	write_all_common_impl([t](std::byte const* ilast ifirst,std::byte const* ilast)
+	{
+		return write_some_bytes_define(ifirst,ilast);
+	});
+}
+
+template<typename T>
+inline constexpr void decay_write_all_impl(T t,io_scatter_t const* scatters,::std::size_t n)
+{
+	for(auto i{scatters},e{i+n};i!=e;++i)
+	{
+		decay_write_all_impl(t,i->base,i->len);
+	}
+}
+
+
+}
+
 namespace operations
 {
 
-inline constexpr void write_all()
+template<typename output,std::integral char_type>
+inline constexpr void write_all(output&& out,char_type const* first,char_type const* last)
 {
-	
+	using nocvref_output = std::remove_cvref_t<output>;
+	if constexpr(buffer_output_stream<nocvref_output>)
+	{
+
+	}
+	else if constexpr(write_some_bytes_stream<nocvref_output>)
+	{
+		::fast_io::details::write_all_common_impl(write_some_bytes_proxy(out),
+			reinterpret_cast<std::byte const*>(first),
+			reinterpret_cast<std::byte const*>(last));
+	}
+}
+
+template<typename output>
+inline constexpr void scatter_write_bytes_all(output&& out,io_scatters_t scatters)
+{
+	using nocvref_output = std::remove_cvref_t<output>;
+	if constexpr(write_some_bytes_stream<nocvref_output>)
+	{
+		::fast_io::details::write_all_common_impl(write_some_bytes_proxy(out),
+			reinterpret_cast<std::byte const*>(first),
+			reinterpret_cast<std::byte const*>(last));
+	}
+}
+
+template<typename output>
+inline constexpr void scatter_write_all(output&& out,io_scatters_t scatters)
+{
+	using nocvref_output = std::remove_cvref_t<output>;
+	if constexpr(write_some_bytes_stream<nocvref_output>)
+	{
+		::fast_io::details::write_all_common_impl(write_some_bytes_proxy(out),
+			reinterpret_cast<std::byte const*>(first),
+			reinterpret_cast<std::byte const*>(last));
+	}
 }
 
 }
