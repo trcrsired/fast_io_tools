0b5ac4ea04a09a2da738865efbfdcf0d989f3681
trcrsired <uwgghhbcad@gmail.com>
2023-02-27 17:35:06 -0500

    digits

--- myitoa/main.cc
+++ myitoa/main.cc
@@ -4,7 +4,7 @@
 int main()
 {
 	fast_io::out_buf_type obf{fast_io::out()};
-	for(auto ch : ::fast_io::details::digits_table<char8_t,16,true>)
+	for(auto ch : ::fast_io::details::digits_table<wchar_t,10,true>)
 	{
 		print(obf,fast_io::mnp::chvw(ch));
 	}
