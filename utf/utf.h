e29dcd59bc88d6fb39a6eeddc6534c9085d5eb2a
trcrsired <uwgghhbcad@gmail.com>
2023-06-20 00:24:42 -0400

    fix utf

--- utf/utf.h
+++ utf/utf.h
@@ -64,62 +64,81 @@ inline constexpr deco_result<char8_t,char32_t> utf8_to_utf32_simd_impl(
 			tofirst+=16;
 			continue;
 		}
-		if constexpr(false)
-		{
-			fromfirst+=16;
-			tofirst+=16;
-		}
-		else
-		{
-			unsigned czv{vector_mask_countr_zero(res)};
+		unsigned czv{vector_mask_countr_zero(res)};
 #if __has_cpp_attribute(assume)
-			[[assume(czv<16)]];
+		[[assume(czv<16)]];
 #endif
-			for(unsigned i{};i!=czv;++i)
+		{
+			unsigned i{};
+			if(3<czv)
+			{
+				ret.value=__builtin_shufflevector(simvec.value,zeros.value,0,16,16,16,1,16,16,16,
+					2,16,16,16,3,16,16,16);
+				ret.store(tofirst);
+				i=4;
+			}
+			if(7<czv)
+			{
+				ret.value=__builtin_shufflevector(simvec.value,zeros.value,4,16,16,16,5,16,16,16,
+					6,16,16,16,7,16,16,16);
+				ret.store(tofirst+4);
+				i=8;
+			}
+			if(11<czv)
+			{
+				ret.value=__builtin_shufflevector(simvec.value,zeros.value,8,16,16,16,9,16,16,16,
+					10,16,16,16,11,16,16,16);
+				ret.store(tofirst+8);
+				i=12;
+			}
+#if __has_cpp_attribute(assume)
+			[[assume(czv-i<4)]];
+#endif
+			for(;i!=czv;++i)
 			{
 				*tofirst=*fromfirst;
 				++fromfirst;
 				++tofirst;
 			}
-			::std::uint_least32_t val;
-			__builtin_memcpy(__builtin_addressof(val),fromfirst,sizeof(val));
-			char unsigned v;
-			if constexpr(::std::endian::little==::std::endian::native)
-			{
-				v=static_cast<char unsigned>(val);
-				val=::std::byteswap(val);
-			}
-			else
-			{
-				v=static_cast<char unsigned>(val>>24u);
-			}
-			int length{::std::countl_one(v)};
-			if(4<length)
-			{
-				*tofirst=0xFFFD;
-				++fromfirst;
-				++tofirst;
-				continue;
-			}
-			auto lengthm2{length-2};
-			::std::uint_least32_t mask{utf8masks[lengthm2]};
-			::std::uint_least32_t maskcond{utf8maskscond[lengthm2]};
-			if((val&mask)!=maskcond)
-			{
-				*tofirst=0xFFFD;
-				fromfirst+=length;
-				++tofirst;
-				continue;
-			}
-			val&=utf8masks2[lengthm2];
-			val>>=(static_cast<unsigned>(2-lengthm2)<<3);
-			*tofirst=(val&0xFF)|
-				((val&0xFF00)>>2)|
-				((val&0xFF0000)>>4)|
-				((val&0xFF000000)>>6);
+		}
+		::std::uint_least32_t val;
+		__builtin_memcpy(__builtin_addressof(val),fromfirst,sizeof(val));
+		char unsigned v;
+		if constexpr(::std::endian::little==::std::endian::native)
+		{
+			v=static_cast<char unsigned>(val);
+			val=::std::byteswap(val);
+		}
+		else
+		{
+			v=static_cast<char unsigned>(val>>24u);
+		}
+		int length{::std::countl_one(v)};
+		if(4<length)
+		{
+			*tofirst=0xFFFD;
+			++fromfirst;
+			++tofirst;
+			continue;
+		}
+		auto lengthm2{length-2};
+		::std::uint_least32_t mask{utf8masks[lengthm2]};
+		::std::uint_least32_t maskcond{utf8maskscond[lengthm2]};
+		if((val&mask)!=maskcond)
+		{
+			*tofirst=0xFFFD;
 			fromfirst+=length;
 			++tofirst;
+			continue;
 		}
+		val&=utf8masks2[lengthm2];
+		val>>=(static_cast<unsigned>(2-lengthm2)<<3);
+		*tofirst=(val&0xFF)|
+			((val&0xFF00)>>2)|
+			((val&0xFF0000)>>4)|
+			((val&0xFF000000)>>6);
+		fromfirst+=length;
+		++tofirst;
 	}
 	return {fromfirst,tofirst};
 }
