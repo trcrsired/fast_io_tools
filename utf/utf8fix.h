be2dd4407be275ed2021feab1074c472cf387a48
trcrsired <uwgghhbcad@gmail.com>
2023-07-03 01:44:40 -0400

    mmm

--- utf/utf8fix.h
+++ utf/utf8fix.h
@@ -21,35 +21,23 @@ inline constexpr deco_result_t<char_type,to_char_type> codecvtremain(code_cvt_un
 					to_char_type* tolast,
 					F fn)
 {
-	if(cvt.remained!=0)
+	auto remn{cvt.remained};
+	if(remn!=0)
 	{
 		::std::size_t diff{static_cast<::std::size_t>(fromlast-fromfirst)};
-		::std::uint_least8_t nv{N-cvt.remained};
-		bool diffsmcvtrem{diff<nv};
-		if(!diffsmcvtrem)
-		{
-			diff=nv;
+		::std::size_t remnspace{N-remn};
+		if(remnspace<diff)
+		{			
 		}
-		if(diffsmcvtrem)
-		{
-			cvt.remained=::fast_io::details::non_overlapped_copy_n(fromfirst,diff,cvt.buffer+cvt.remained)-cv.buffer;
-			return {fromlast,tofirst};
-		}
-		auto [fromit,toit] = fn(cvt.buffer,cvt.buffer+N,tofirst,tolast);
-		fromfirst+=diff;
-		if(toit==tolast)
-		{
-			cvt.remained=::fast_io::freestanding::copy(fromit,cvt.buffer+N,cvt.buffer)-cvt.buffer;
-			return {fromfirst,tolast};
-		}
-		cvt.remained=0;
 	}
 	auto [fromit,toit] = fn(fromfirst,fromlast,tofirst,tolast);
 	if(toit==tolast)
 	{
 		return {fromit,toit};
 	}
-	cvt.remained=::fast_io::details::non_overlapped_copy(fromit,fromlast,cvt.buffer)-cvt.buffer;
+	cvt.remained=
+		static_cast<::std::uint_least8_t>(
+			::fast_io::details::non_overlapped_copy(fromit,fromlast,cvt.buffer)-cvt.buffer);
 	return {fromlast,toit};
 }
 
