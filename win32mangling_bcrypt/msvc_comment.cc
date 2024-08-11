a9d1021b46e763d494753d84cd120b955b33577d
trcrsired <uwgghhbcad@gmail.com>
2024-08-11 01:52:32 -0400

    gg

--- win32mangling_bcrypt/msvc_comment.cc
+++ win32mangling_bcrypt/msvc_comment.cc
@@ -1,19 +1,26 @@
+#include<string>
 #include<string_view>
 #include<fast_io_device.h>
 #include<fast_io.h>
 
 int main(int argc,char** argv)
 {
+	using namespace ::fast_io::io;
 	if(argc<3)
 	{
+		if(argc==0)
+		{
+			return 1;
+		}
 		perr("Usage: ",fast_io::mnp::os_c_str(argv[0])," <input file> <output file>\n");
 		return 1;
 	}
 	fast_io::u8ibuf_file ibf(fast_io::mnp::os_c_str(argv[1]));
 	fast_io::u8obuf_file obf(fast_io::mnp::os_c_str(argv[2]));
-	print(obf,u8"#pragma once\n\n");
-	for(std::u8string_view line:line_scanner(ibf))
+	print(obf,u8"#pragma once\n\n// clang-format off\n");
+	for(::std::u8string linesc;scan<true>(ibf, ::fast_io::mnp::line_get(linesc));)
 	{
+		std::u8string_view line{linesc};
 		if(line.empty())
 			continue;
 		auto i{line.rfind(u8' ')};
@@ -24,4 +31,5 @@ int main(int argc,char** argv)
 		}
 		print(obf,u8"#pragma comment(linker,\"/alternatename:",line.substr(0,i),u8"=",line.substr(i+1),u8"\")\n");
 	}
+	print(obf,u8"// clang-format on\n");
 }
