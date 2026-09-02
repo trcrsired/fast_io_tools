ee590f12ba976b142208999ac82e899294dd3724
trcrsired <uwgghhbcad@gmail.com>
2026-09-02 21:21:08 +0800

    try to get simple code work for C++26 varadics template for loop

--- print_freestanding_cxx26/print_freestanding_cxx26.h
+++ print_freestanding_cxx26/print_freestanding_cxx26.h
@@ -1,5 +1,31 @@
 #pragma once
 
+namespace fast_io::details
+{
+
+template<::std::size_t first, ::std::size_t last>
+inline constexpr auto create_index_array_range() noexcept
+{
+    ::fast_io::freestanding::array<::std::size_t, static_cast<::std::size_t>(last-first)> arr;
+    ::std::size_t i{first};
+    for(auto& e : arr)
+    {
+        e = i;
+        ++i;
+    }
+    return arr;
+}
+
+template<::std::size_t n>
+inline constexpr auto create_index_array() noexcept
+{
+    constexpr auto arr{::fast_io::details::create_index_array_range<0,n>()};
+    return arr;
+}
+
+
+}
+
 namespace fast_io::operations::decay
 {
 template <bool line, typename outputstmtype, typename... Args>
@@ -37,24 +63,29 @@ inline constexpr decltype(auto) print_freestanding_decay2(outputstmtype optstm, 
 #endif
 	{
 		constexpr bool is_buffer_output_stream{::fast_io::operations::decay::defines::has_obuffer_basic_operations<outputstmtype>};
-		template for(auto const i: ::std::make_index_sequence<sizeof...(Args)>)
+		template for(auto const i: ::fast_io::details::create_index_array<sizeof...(Args)>())
 		{
-			using ArgsIType = Args...[i];
+
+            using ArgsIType = Args...[i];
 			constexpr bool islastwithlf{line&&(i+1zu == sizeof...(Args))};
 			if constexpr(::fast_io::reserve_printable<output_char_type, ArgsIType>)
 			{
 				constexpr ::std::size_t reserved_space{print_reserve_size(::fast_io::io_reserve_type<output_char_type, ArgsIType>)
 					+ static_cast<::std::size_t>(islastwithlf)};
 				static_assert(reserved_space<=::std::numeric_limits<::std::ptrdiff_t>::max(), "reserve space too big");
-				constexpr ::std::ptrdiff_t ptrdiff_reserved_space{static_cast<::std::ptrddiff_t>(reserved_space)};
-				output_char_type buffer[reserved_space] [[indeterminate]];
+				constexpr ::std::ptrdiff_t ptrdiff_reserved_space{static_cast<::std::ptrdiff_t>(reserved_space)};
+				output_char_type buffer[reserved_space]
+#if __has_cpp_attribute(indeterminate)
+                [[indeterminate]]
+#endif
+                ;
 				auto first{buffer};
 				auto it{buffer};
 				bool buffer_space_enough{};
 				if constexpr (is_buffer_output_stream)
 				{
-					auto curr=obuffer_curr(out);
-					auto end=obuffer_end(out);
+					auto curr=obuffer_curr(optstm);
+					auto end=obuffer_end(optstm);
 					buffer_space_enough = (ptrdiff_reserved_space < end-curr);
 				}
 				it = print_reserve_define(::fast_io::io_reserve_type<output_char_type, ArgsIType>, it, args...[i]);
