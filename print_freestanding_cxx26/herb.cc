42a0271abe5684fd337e63402e9a16727a837251
trcrsired <uwgghhbcad@gmail.com>
2026-09-15 06:30:31 +0800

    herb

---
+++ print_freestanding_cxx26/herb.cc
@@ -0,0 +1,33 @@
+#include <fast_io.h>
+#include <fast_io_dsal/array.h>
+#ifdef __HERBCEPTIONS__
+#define FAST_IO_HERBCEPTIONS_THROWS throws
+#define FAST_IO_HERBCEPTIONS_THROWS_IF(x) throws(x)
+#else
+#define FAST_IO_HERBCEPTIONS_THROWS
+#define FAST_IO_HERBCEPTIONS_THROWS_IF(x) noexcept(!(x))
+#endif
+
+#if __has_cpp_attribute(indeterminate)
+#define FAST_IO_INDETERMINATE [[indeterminate]]
+#else
+#define FAST_IO_INDETERMINATE
+#endif
+
+
+#if 1
+#include "print_freestanding_cxx26.h"
+#endif
+
+int main()
+try
+{
+	throw throws::std::errc::network_down;
+}
+catch throws(::std::error e)
+{
+	::fast_io::posix_io_observer myiob{2};
+	::fast_io::operations::decay::print_freestanding_decay2<true>(myiob,
+																  ::fast_io::basic_io_scatter_t<char>{"hello\t", 6}, ::fast_io::mnp::name(e));
+	return 1;
+}
